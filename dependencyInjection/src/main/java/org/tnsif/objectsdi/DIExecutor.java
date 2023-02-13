f0af5d9971b203b1a1bec30d15572a198cbd60a1
bhuvaneshwari <bhuvaneshwaribk2000@gmail>
2023-02-13 17:23:05 +0530

    second commit

---
+++ dependencyInjection/src/main/java/org/tnsif/objectsdi/DIExecutor.java
@@ -0,0 +1,17 @@
+package org.tnsif.objectsdi;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DIExecutor {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext b=new ClassPathXmlApplicationContext("web.xml");
+		Student e=b.getBean("e1",Student.class);
+		e.print();
+		
+
+	}
+
+}
