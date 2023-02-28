6bb95e4bfe0b2bb80a45792f94494fa2ff444241
bhuvaneshwari <bhuvaneshwaribk2000@gmail>
2023-02-28 17:35:28 +0530

    first commit

---
+++ PlacementMangement-2/src/main/java/org/tnsif/demo/PlacementMangement2Application.java
@@ -0,0 +1,13 @@
+package org.tnsif.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementMangement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementMangement2Application.class, args);
+	}
+
+}
---
+++ PlacementMangement-2/src/main/java/org/tnsif/demo/PlacementRepository.java
@@ -0,0 +1,9 @@
+package org.tnsif.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlacementRepository extends JpaRepository<Placement,Integer>{
+
+	
+
+}
